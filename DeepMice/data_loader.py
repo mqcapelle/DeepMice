e78dd2699b56eed30f6ff9c481797daffb15f97f
chengfanbrain <77146522+chengfanbrain@users.noreply.github.com>
2021-08-09 14:09:10 +0900

    [Update] update data_loader (import module)

--- DeepMice/data_loader.py
+++ DeepMice/data_loader.py
@@ -1,11 +1,10 @@
+import numpy as np
 
 
 def load_example_data(path='one_example_session.npy', print_keys=True):
   """ Example function to load the exported data
-
   Input:
     path: str (path to the downloaded file)
-
   Output:
     data: dict ()
   """
@@ -21,12 +20,10 @@ def load_example_data(path='one_example_session.npy', print_keys=True):
 def get_trial_matrix_3d(activity, neuron_time, stimulus_details,
                         nr_frames_after=10):
   """Calculate 3D trial matrix (trials,neurons,time) from loaded data
-
   Input:
     activity: 2d matrix (neurons, time)
     neuron_time: 1d matrix (time)
     stimulus_details: pandas dataframe (as loaded from one_example_session.npy)
-
   Output:
     trial_matrix_3d: (trials, neurons, time)
     image_idx: (trials) Image number that is shown (8 for omitted)
@@ -38,6 +35,7 @@ def get_trial_matrix_3d(activity, neuron_time, stimulus_details,
 
   trial_matrix_3d = np.zeros( (nr_trials, nr_neurons, nr_frames))
   image_index = np.zeros( nr_trials )
+  is_change = np.zeros(nr_trials)
 
   for i in range(nr_trials):
     stim_row = stimulus_details.iloc[i+1]  # skip first entry
@@ -45,8 +43,8 @@ def get_trial_matrix_3d(activity, neuron_time, stimulus_details,
 
     start_idx = np.argmin( np.abs( neuron_time - start_time))
     trial_matrix_3d[i,:,:] = activity[:,start_idx:start_idx+nr_frames]
-    
+
     image_index[i] = stim_row.image_index
+    is_change[i] = stim_row.is_change
 
-  return trial_matrix_3d, image_index
-
+  return trial_matrix_3d, image_index, is_change
