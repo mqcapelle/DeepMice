3393595d05b79d8ab863d44fa8c22a70197ac6d0
mqcapelle <45564498+mqcapelle@users.noreply.github.com>
2021-08-16 11:55:38 +0200

    data_loader_class.py: Shivani somehow managed to get it working. Don't ask her how it actually works.

--- DeepMice/utils/data_loader_class.py
+++ DeepMice/utils/data_loader_class.py
@@ -8,47 +8,96 @@ import warnings
 import numpy as np
 import xarray as xr
 import torch
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader, TensorDataset, Dataset
 from tqdm import tqdm
+
+# Local library imports
+from DeepMice.helpers.helpers import set_seed, set_device
+
+class my_dataset(Dataset):
+    def __init__(self, data, label):
+        self.data = data
+
+        self.label = label
+
+    def __getitem__(self, index):
+        return self.data[:,:,index], self.label[:,:,index]
+
+    def __len__(self):
+        return self.data.shape[2]
 
 
 class DeepMiceDataLoader:
     def __init__(
             self,
-            path_to_data_folder,
+            path_to_data_file,
             batch_size=128, output='image_index',
-            test_ratio=0.2, split_type='block_middle',
+            ratio=(0.8, 0.1, 0.1), trial_length=0.7,
+            split_type='block_middle',
             with_time=True, return_all=False,
-            min_number_of_neurons=400, max_number_of_neurons=1000,
-            max_number_of_timestamps=None, max_number_of_sessions=None,
-            shuffle_trials=False, shuffle_neurons=False,
-            rng=None
+            # min_number_of_neurons=400, max_number_of_neurons=1000,
+            # max_number_of_timestamps=None, max_number_of_sessions=None,
+            # shuffle_trials=False, shuffle_neurons=False,
+            seed=None
     ):
 
-        self.path_to_data_folder = path_to_data_folder
-        self.path_to_data_files = list(self.path_to_data_folder.glob("*.nc"))
+        self.path_to_data_file = path_to_data_file
 
         self.batch_size = batch_size
-        self.test_ratio = test_ratio
+        self.ratio = ratio
+        self.trial_length = trial_length
 
         self.output = output
         self.split_type = split_type
         self.with_time = with_time
         self.return_all = return_all
 
-        self.min_number_of_neurons = min_number_of_neurons
-        self.max_number_of_neurons = max_number_of_neurons
-        self.max_number_of_timestamps = max_number_of_timestamps
-        self.max_number_of_sessions = max_number_of_sessions
+        # self.min_number_of_neurons = min_number_of_neurons
+        # self.max_number_of_neurons = max_number_of_neurons
+        # self.max_number_of_timestamps = max_number_of_timestamps
+        # self.max_number_of_sessions = max_number_of_sessions
 
 
         self.shuffle_trials = shuffle_trials
         self.shuffle_neurons = shuffle_neurons
 
-        if rng is None:
-            self.rng = np.random.default_rng(seed=2021)
+        self.seed = seed
+        self.num_workers = 40
 
-    def load_single_session(self, path=None):
+
+    def setup(self):
+
+        set_seed(seed=self.seed)
+        # DEVICE = set_device()
+
+        self.load_single_session()  # Load data xarray
+        self.nr_frames_after = int(self.data.attrs['frame_rate_Hz'] * self.trial_length)
+        self.get_trial_matrix_3d()
+
+        self.trial_matrix_3d = torch.tensor(self.trial_matrix_3d).permute(1, 2, 0)
+        self.y = self.y.reshape((1, 1, -1))
+
+        self.dataset = my_dataset(self.trial_matrix_3d, self.y)  # (trials, neurons, timestamps)
+        #validation_split = .1
+
+        #testing_split = .2
+        shuffle_dataset = True
+
+        # Creating data indices for training and validation splits:
+        dataset_size = len(self.dataset)
+
+        train_size = int(0.8 * dataset_size)
+        test_size = dataset_size - train_size
+        self.train_dataset, self.test_dataset = torch.utils.data.random_split(self.dataset, [train_size, test_size])
+
+        train_size = int(0.9 * len(self.train_dataset))
+        val_size = len(self.train_dataset) - train_size
+
+        self.train_dataset, self.val_dataset = torch.utils.data.random_split(self.train_dataset, [train_size, val_size])
+        self.create_dataloader()
+
+
+    def load_single_session(self):
         """ Load one session from file
 
         Input:
@@ -57,10 +106,7 @@ class DeepMiceDataLoader:
           data: xarray with data for one session
         """
 
-        if path is None:
-            path = self.path_to_data_files[0]
-
-        return xr.open_dataset(path)
+        self.data = xr.open_dataset(self.path_to_data_file)
 
     def easy_train_test_loader(self, data, trial_length=0.7):
         """
@@ -153,7 +199,7 @@ class DeepMiceDataLoader:
 
             # Only use behavior sessions with correct amount of neurons
             number_of_neurons = len(data.neuron_id)
-            if number_of_neurons < self.min_number_of_neurons or number_of_neurons > max_number_of_neurons:
+            if number_of_neurons < self.min_number_of_neurons or number_of_neurons > self.max_number_of_neurons:
                 continue
 
             # Stop if max_number_of_sessions is reached
